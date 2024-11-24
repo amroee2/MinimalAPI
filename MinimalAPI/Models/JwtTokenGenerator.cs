19b8784d8f6078eb8f5592c75d5828706866b3b2
Amro <amroqadadha@gmail.com>
2024-11-24 19:19:38 +0200

    Add project files.

---
+++ MinimalAPI/Models/JwtTokenGenerator.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace MinimalAPI.Models
+{
+    public class JwtTokenGenerator
+    {
+
+        public string GenerateJwtToken(string username, string password)
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("This is a secret key");
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new System.Security.Claims.Claim[]
+                {
+                    new System.Security.Claims.Claim(System.Security.Claims.ClaimTypes.Name, username)
+                }),
+                Expires = System.DateTime.UtcNow.AddMinutes(5),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}

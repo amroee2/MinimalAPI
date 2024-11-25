e8fc2b11d38fb26d21def21683761e49210b39f9
Amro <amroqadadha@gmail.com>
2024-11-25 13:44:30 +0200

    Inject new dependency

--- MinimalAPI/Program.cs
+++ MinimalAPI/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.IdentityModel.Tokens;
+using MinimalAPI.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<ITokenGenerator, JwtTokenGenerator>();
 
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 builder.Services.AddAuthentication("Bearer")
